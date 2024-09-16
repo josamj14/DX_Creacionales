dc89d7bd04f5841de905af62a71fac64a4f0e9b9
Marbelbb <ma.brenes@estudiantec.cr>
2024-09-16 03:36:52 -0600

    Pagina Principal y ajustes css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,22 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AbstractFactoryComponent } from "./abstract-factory/abstract-factory.component";
+import { ObjectPoolComponent } from './object-pool/object-pool.component';
+import { PrototypeComponent } from './prototype/prototype.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: 'video', component: AbstractFactoryComponent}
+    { path: '', component: HomeComponent },
+    {path: 'abstract-factory', component: AbstractFactoryComponent},
+    { path: 'pool-objects', component: ObjectPoolComponent },
+    { path: 'prototype', component: PrototypeComponent }
 ];
+
+@NgModule({
+    
+    imports: [RouterModule.forRoot(routes), RouterModule],
+    exports: [RouterModule],
+
+  })
+  export class AppRoutingModule { }
