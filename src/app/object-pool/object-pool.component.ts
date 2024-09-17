8b84247c3d1536b7375457ded37733a99967efcb
josi marin <marinjosi15@gmail.com>
2024-09-17 00:16:05 -0600

    Grafico de object pool

--- src/app/object-pool/object-pool.component.ts
+++ src/app/object-pool/object-pool.component.ts
@@ -1,16 +1,88 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-object-pool',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './object-pool.component.html',
   styleUrl: './object-pool.component.css'
 })
-export class ObjectPoolComponent {
 
+export class ObjectPoolComponent {
+  //Esta clase es el main que tiene que controla la interfaz gráfica y llamadas a métodos de los objetos
+  countClientes = 1;
+  chatList : Chat[] = [];
+  piscina : boolean = false;
+
+  inicializarPiscina(){
+    OperadoresPool.inicializarPiscina();
+    this.piscina = true;
+  }
+
+  crearChat(){
+    var id : number = this.countClientes++;;
+    var chatCreado : Chat = new Chat (`${id}`, new Cliente(`${id}`, "Cliente "+ id));
+    chatCreado.iniciarChat();
+    this.chatList.push(chatCreado);
+  }
+
+  getMessage(chat : Chat):string{
+    
+    if (typeof chat.operador === 'undefined') {
+      return "El chat del " + chat.cliente.nombre + " no pudo ser creado por falta de operadores.";
+    }
+    return "El chat " + chat.id + " tiene al " + chat.cliente.nombre + " y se le asigno al " + chat.operador?.nombre;
+    
+  }
+
+  eliminarChat(chat : Chat){
+    chat.terminarChat();
+    this.chatList.filter(c => c !== chat);
+  }  
+
+  exists(chat: Chat) : boolean {
+    return chat.exist;
+  }
+
+  operadorExists(chat: Chat) : boolean {
+    return (typeof chat.operador !== 'undefined')
+  }
 }
 
+<<<<<<< Updated upstream
+=======
+class OperadoresPool{
+  static opDisponibles: Operador[]  = [];
+  static opOcupados: Operador[]  = [];
+
+  constructor(){
+  }
+
+  static inicializarPiscina() {
+    for (let index = 0; index < 15; index++) {
+      var opCreado : Operador =new Operador(`${index+1}`,"Operador " + (index+1));
+      OperadoresPool.opDisponibles.push(opCreado);
+    }
+    console.log("Piscina inicializada!");
+  }
+
+  static asignarOperador(): Operador | undefined {
+    var opAsignar = this.opDisponibles.pop();
+    if (opAsignar!== undefined) {
+      OperadoresPool.opOcupados.push(opAsignar);
+    }
+    console.log("Se asignará al "+ opAsignar?.nombre);
+    return opAsignar;
+  }
+
+  static liberarOperador(op : Operador) {
+    this.opDisponibles.push(op);
+    OperadoresPool.opOcupados = OperadoresPool.opOcupados.filter(Operador => Operador !== op); 
+    console.log("Se liberó al " + op.nombre);
+  }
+}
+
+>>>>>>> Stashed changes
 class Cliente{
   id : string;
   nombre : string;
@@ -19,6 +91,14 @@ class Cliente{
     this.id = id;
     this.nombre = nombre;
   }
+<<<<<<< Updated upstream
+=======
+
+  enviarMensaje(mensaje : string, chat: Chat){
+    chat.agregarMensaje(mensaje);
+  }
+
+>>>>>>> Stashed changes
 }
 
 class Operador{
@@ -29,6 +109,14 @@ class Operador{
     this.id = id;
     this.nombre = nombre;
   }
+<<<<<<< Updated upstream
+=======
+
+  enviarMensaje(mensaje : string, chat:Chat){
+    chat.agregarMensaje(mensaje);
+  }
+
+>>>>>>> Stashed changes
 }
 
 class Chat{
@@ -36,11 +124,36 @@ class Chat{
   cliente: Cliente;
   operador?: Operador;
   mensajes: string[];
+  exist: boolean = true;
 
   constructor(id: string, cliente: Cliente){
     this.id = id;
     this.cliente = cliente;
     this.mensajes = [];
+    console.log("Chat was created with " + cliente.nombre);
   }
+<<<<<<< Updated upstream
+=======
+
+  iniciarChat(){
+    var op : Operador | undefined = OperadoresPool.asignarOperador();
+    if (typeof op === 'undefined') {
+      console.log("Chat could not be created");
+    }
+    this.operador = op;
+  }
+
+  terminarChat(){
+    if (typeof this.operador !== 'undefined') {
+      OperadoresPool.liberarOperador(this.operador);
+    }
+    this.exist = false;
+  }
+
+  agregarMensaje(mensaje:string){
+    this.mensajes.push(mensaje)
+  }
+
+>>>>>>> Stashed changes
 }
 
